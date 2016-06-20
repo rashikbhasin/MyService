12869688594a5d4f8bf90f2d9abe8ee1e6757193
Rashik Bhasin <rashikbhasin@gmail.com>
2016-06-20 18:00:56 +0530

    MyService App Added

---
+++ app/src/main/java/com/rashikbhasingmail/myservice/MainActivity.java
@@ -0,0 +1,32 @@
+package com.rashikbhasingmail.myservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
